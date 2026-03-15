2504398d0def7220d6d551d8f170ee86859fe56a
Theo Farnole <31354550+Theo-Farnole@users.noreply.github.com>
2026-03-15 17:19:08 +0100

    add thumbnail to ReferenceThumbnail

--- src/components/ReferenceThumbnail.tsx
+++ src/components/ReferenceThumbnail.tsx
@@ -1,23 +1,70 @@
 import React from "react";
 import { MDReference } from "../data/references";
+import "./ReferenceThumbnail.scss";
 
-type LinkItemProps = {
+type ReferenceThumbnailProps = {
   reference: MDReference;
 };
 
-export const ReferenceThumbnail: React.FC<LinkItemProps> = ({ reference }) => {
+const getYoutubeThumbnailUrl = (url: string): string | null => {
+  try {
+    const parsed = new URL(url);
+
+    // Standard YouTube URL: https://www.youtube.com/watch?v=VIDEO_ID
+    const vParam = parsed.searchParams.get("v");
+    if (vParam) {
+      return `https://img.youtube.com/vi/${vParam}/hqdefault.jpg`;
+    }
+
+    // Short URL: https://youtu.be/VIDEO_ID
+    if (parsed.hostname === "youtu.be") {
+      const id = parsed.pathname.replace("/", "");
+      if (id) {
+        return `https://img.youtube.com/vi/${id}/hqdefault.jpg`;
+      }
+    }
+
+    return null;
+  } catch {
+    return null;
+  }
+};
+
+export const ReferenceThumbnail: React.FC<ReferenceThumbnailProps> = ({
+  reference,
+}) => {
+  const thumbnailUrl = getYoutubeThumbnailUrl(reference.url);
+
   return (
     <tr>
+      <td>
+        {thumbnailUrl && (
+          <a href={reference.url} target="_blank" rel="noreferrer">
+            <img
+              src={thumbnailUrl}
+              alt={`Thumbnail for ${reference.url}`}
+              className="reference-thumbnail__image"
+            />
+          </a>
+        )}
+      </td>
       <td>
         <a href={reference.url} target="_blank" rel="noreferrer">
           {reference.url}
         </a>
+        <div className="reference-thumbnail__timecode">{reference.timecode}</div>
       </td>
-      <td>{reference.timecode}</td>
       <td>{reference.author}</td>
-      <td>{reference.tags.join(", ")}</td>
+      <td>
+        <div className="reference-thumbnail__tags">
+          {reference.tags.map((tag) => (
+            <span key={tag} className="reference-thumbnail__tag">
+              {tag}
+            </span>
+          ))}
+        </div>
+      </td>
     </tr>
   );
 };
-
 
